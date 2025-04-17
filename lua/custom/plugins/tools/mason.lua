34b43715bee1310cb59caba9247ec6e5fc68dd78
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2025-04-17 20:54:55 +0200

    feat: Add linters

--- lua/custom/plugins/tools/mason.lua
+++ lua/custom/plugins/tools/mason.lua
@@ -1,52 +1,54 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-      local mason = require("mason")
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			local mason = require("mason")
 
-      local mason_lspconfig = require("mason-lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
 
-      local mason_tool_installer = require("mason-tool-installer")
+			local mason_tool_installer = require("mason-tool-installer")
 
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 
-      mason_lspconfig.setup({
-        ensure_installed = {
-          "dockerls",
-          "docker_compose_language_service",
-          "gopls",
-          "clangd",
-          "lua_ls",
-          "bashls",
-          "basedpyright",
-        },
-        automatic_installation = true,
-      })
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"dockerls",
+					"docker_compose_language_service",
+					"gopls",
+					"clangd",
+					"lua_ls",
+					"bashls",
+					"basedpyright",
+				},
+				automatic_installation = true,
+			})
 
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "clang-format",
-          "gofumpt",
-          "goimports-reviser",
-          "staticcheck",
-          "prettier",
-          "stylua",
-          "ruff",
-          "rust-analyzer",
-        },
-      })
-    end,
-  }
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"clang-format",
+					"gofumpt",
+					"goimports-reviser",
+					"staticcheck",
+					"prettier",
+					"stylua",
+					"ruff",
+					"hadolint",
+					"jsonlint",
+					"rust-analyzer",
+				},
+			})
+		end,
+	},
 }
