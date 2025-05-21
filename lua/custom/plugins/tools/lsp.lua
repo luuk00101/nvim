8a1f566a7b8aa92f645ef56063931d33668d771d
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2025-05-21 21:38:01 +0200

    feat: Add odin LSP

---
+++ lua/custom/plugins/tools/lsp.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = function()
+			-- To enable diagnostic virtual text (disabled by default??)
+			vim.diagnostic.config({
+				virtual_text = true,
+				float = { border = "rounded" },
+			})
+
+			-- Keymaps
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+
+			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.get_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.get_next)
+
+			vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>")
+
+			-- Enable LSPs
+			local enabled_lsps = {
+				"lua_ls",
+				"basedpyright",
+				"ruff",
+				"gopls",
+				"bashls",
+				"clangd",
+				"dockerls",
+				"docker_compose_language_service",
+				"ols",
+			}
+
+			for _, lsp in ipairs(enabled_lsps) do
+				vim.lsp.enable(lsp)
+			end
+		end,
+	},
+}
--- lua/custom/plugins/tools/mason.lua
+++ lua/custom/plugins/tools/mason.lua
@@ -3,16 +3,9 @@ return {
 		"williamboman/mason.nvim",
 		dependencies = {
 			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
-			local mason = require("mason")
-
-			local mason_lspconfig = require("mason-lspconfig")
-
-			local mason_tool_installer = require("mason-tool-installer")
-
-			mason.setup({
+			require("mason").setup({
 				ui = {
 					icons = {
 						package_installed = "✓",
@@ -22,7 +15,7 @@ return {
 				},
 			})
 
-			mason_lspconfig.setup({
+			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"dockerls",
 					"docker_compose_language_service",
@@ -31,23 +24,9 @@ return {
 					"lua_ls",
 					"bashls",
 					"basedpyright",
+					"ols",
 				},
 				automatic_installation = true,
-			})
-
-			mason_tool_installer.setup({
-				ensure_installed = {
-					"clang-format",
-					"gofumpt",
-					"goimports-reviser",
-					"staticcheck",
-					"prettier",
-					"stylua",
-					"ruff",
-					"hadolint",
-					"jsonlint",
-					"rust-analyzer",
-				},
 			})
 		end,
 	},
