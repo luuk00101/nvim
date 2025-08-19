d9d7fac812b6b9c82c66da085aadbbe228334409
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2025-08-19 09:19:05 +0200

    feat: add lazy keymap

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -17,12 +17,6 @@ vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>")
 
--- Window navigation
-vim.keymap.set("n", "<c-j>", "<c-w><c-j>")
-vim.keymap.set("n", "<c-k>", "<c-w><c-k>")
-vim.keymap.set("n", "<c-l>", "<c-w><c-l>")
-vim.keymap.set("n", "<c-h>", "<c-w><c-h>")
-
 -- Clipboard
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true })
 vim.keymap.set("n", "<leader>Y", '"+yg_', { noremap = true })
@@ -30,3 +24,6 @@ vim.keymap.set("n", "<leader>y", '"+y', { noremap = true })
 vim.keymap.set("n", "<leader>yy", '"+yy', { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { noremap = true })
+
+-- Lazy
+vim.keymap.set("n", "<leader>lz", ":Lazy<CR>")
